606879e68714db2e66826a1b52d8d3dbee5f0513
yeni <ngeetl@naver.com>
2023-05-28 16:00:05 +0900

    Edit페이지에서 editTitle, editBody State를 추가해 버튼을 비활성화 시킬 수 있는 로직 구현

--- src/componets/Card.js
+++ src/componets/Card.js
@@ -4,13 +4,13 @@ const Card = ({ post, editHandler, children }) => {
         const printDate = (timeStamp) => {
             return new Date(timeStamp).toLocaleString();
         }    
-
+        
         return (
         <div className='card_wrap' onClick={editHandler}>
             <div className='card'>
                 <div>제목 : {post.title}</div>
                 <div>내용 : {post.body}</div>
-                <small>{printDate(post.createAt)}</small>
+                <small>{post.newCreateAt ? printDate(post.newCreateAt) : printDate(post.createAt)}</small>
             </div>
             {children}
         </div>
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -6,13 +6,17 @@ import LoadingSpinner from '../componets/LoadingSpinner';
 
 const Edit = () => {
     const { id } = useParams();
-
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
+    const [editTitle, setEditTitle] = useState('');
+    const [editBody, setEditBody] = useState('');
     const [loading, setLoading] = useState(true);
 
-    const onChangeTitle = (e) => setTitle(e.target.value);
-    const onChangeBody = (e) => setBody(e.target.value);
+
+
+
+    const onChangeTitle = (e) => setEditTitle(e.target.value);
+    const onChangeBody = (e) => setEditBody(e.target.value);
     const onKeyUp = e => {
         if(e.keyCode === 13) {
             submit(e);
@@ -29,22 +33,25 @@ const Edit = () => {
             alert('본문 내용을 입력하세요');
         } else if((title.length > 1) && (body.length > 1)) {
             axios.put(`http://localhost:3001/posts/${id}`, {
-                title: title,
-                body: body
+                title: editTitle,
+                body: editBody,
+                newCreateAt: Date.now(),
             });
             navigate('/blog');
         }
     }
 
     useEffect(() => {
-        axios.get(`http://localhost:3001/posts/${id}`)
-                .then(res => {
-                    setTitle(res.data.title + '-수정본');
-                    setBody(res.data.body);
-                    setLoading(false);
-                });
-    }, [id]);
-
+                axios.get(`http://localhost:3001/posts/${id}`)
+                        .then(res => {
+                            setTitle(res.data.title + '-수정본');
+                            setBody(res.data.body);
+                            setLoading(false);
+                        });
+                setEditTitle(title);
+                setEditBody(body);
+            }, [id, title, body]);
+            
     const renderEdit = () => {
         if(loading) {
             return <LoadingSpinner/>
@@ -54,7 +61,7 @@ const Edit = () => {
                 <div className='title_wrap post_wrap'>
                     <label for="title">Title</label>
                     <input id="title" 
-                        value={title}
+                        value={editTitle}
                         onChange={onChangeTitle}
                         type="text"
                     />
@@ -62,7 +69,7 @@ const Edit = () => {
                 <div className='body_wrap post_wrap'>
                     <label for="body">Body</label>
                     <textarea id="body" 
-                        value={body} 
+                        value={editBody} 
                         onChange={onChangeBody}
                         onKeyUp={onKeyUp}
                         type="text"
@@ -72,11 +79,23 @@ const Edit = () => {
         )
     }
 
+    const buttonAble = () => {
+        return title === editTitle && body === editBody
+    }
+    console.log(buttonAble())
+
     return (
         <form className='center'> 
             <h3>Edit Page</h3>
             {renderEdit()}
-            <div className='post_button_wrap'><button className="post_button button" onClick={submit}>Edit</button></div>
+            <div className='post_button_wrap'>
+                <button 
+                    className="post_button button" 
+                    onClick={submit}
+                    disabled={buttonAble()}>
+                    Edit
+                </button>
+                </div>
         </form>
     )
 }
